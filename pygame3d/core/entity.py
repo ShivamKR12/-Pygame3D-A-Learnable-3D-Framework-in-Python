81fb6e36bb1564d386587dfb64f287302d669cfc
ShivamKR12 <161696093+ShivamKR12@users.noreply.github.com>
2025-07-13 23:37:10 +0530

    Update entity.py

--- pygame3d/core/entity.py
+++ pygame3d/core/entity.py
@@ -1,1 +1,9 @@
+class Entity:
+    def __init__(self, mesh, shader):
+        self.mesh = mesh
+        self.shader = shader
+
+    def draw(self, camera):
+        self.shader.use()
+        self.mesh.draw()
 
