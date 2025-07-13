8946da6693a43b4fd5549835478e0e46566c6f55
ShivamKR12 <161696093+ShivamKR12@users.noreply.github.com>
2025-07-13 23:41:52 +0530

    Update mesh.py

--- pygame3d/core/mesh.py
+++ pygame3d/core/mesh.py
@@ -1,8 +1,10 @@
 from OpenGL.GL import *
+import numpy as np
 
 class CubeMesh:
     def __init__(self):
-        self.vertices = [
+        vertices = np.array([
+            # positions        # colors
             -1, -1, -1,  1, 0, 0,
              1, -1, -1,  0, 1, 0,
              1,  1, -1,  0, 0, 1,
@@ -11,24 +13,37 @@ class CubeMesh:
              1, -1,  1,  1, 0, 1,
              1,  1,  1,  1, 1, 1,
             -1,  1,  1,  0, 0, 0,
-        ]
-        self.indices = [
+        ], dtype=np.float32)
+
+        indices = np.array([
             0, 1, 2, 2, 3, 0,
             1, 5, 6, 6, 2, 1,
             5, 4, 7, 7, 6, 5,
             4, 0, 3, 3, 7, 4,
             3, 2, 6, 6, 7, 3,
             4, 5, 1, 1, 0, 4
-        ]
-        self.vertices = (GLfloat * len(self.vertices))(*self.vertices)
-        self.indices = (GLuint * len(self.indices))(*self.indices)
+        ], dtype=np.uint32)
+
+        self.vao = glGenVertexArrays(1)
+        glBindVertexArray(self.vao)
+
+        self.vbo = glGenBuffers(1)
+        glBindBuffer(GL_ARRAY_BUFFER, self.vbo)
+        glBufferData(GL_ARRAY_BUFFER, vertices.nbytes, vertices, GL_STATIC_DRAW)
+
+        self.ebo = glGenBuffers(1)
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, self.ebo)
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.nbytes, indices, GL_STATIC_DRAW)
+
+        # Positions
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(0))
+        glEnableVertexAttribArray(0)
+        # Colors
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(12))
+        glEnableVertexAttribArray(1)
+
+        self.count = len(indices)
 
     def draw(self):
-        glEnableClientState(GL_VERTEX_ARRAY)
-        glEnableClientState(GL_COLOR_ARRAY)
-        glVertexPointer(3, GL_FLOAT, 6 * 4, self.vertices)
-        glColorPointer(3, GL_FLOAT, 6 * 4, self.vertices[3:])
-        glDrawElements(GL_TRIANGLES, len(self.indices), GL_UNSIGNED_INT, self.indices)
-        glDisableClientState(GL_VERTEX_ARRAY)
-        glDisableClientState(GL_COLOR_ARRAY)
-
+        glBindVertexArray(self.vao)
+        glDrawElements(GL_TRIANGLES, self.count, GL_UNSIGNED_INT, None)
