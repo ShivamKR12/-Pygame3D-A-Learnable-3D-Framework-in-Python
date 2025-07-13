87c8177d52ee4d3045fed2cbce4c3707d9881baf
ShivamKR12 <161696093+ShivamKR12@users.noreply.github.com>
2025-07-13 23:41:14 +0530

    Update vertex.glsl

--- pygame3d/shaders/vertex.glsl
+++ pygame3d/shaders/vertex.glsl
@@ -1,5 +1,8 @@
-void main() {
-    gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
-    gl_FrontColor = gl_Color;
-}
+varying vec3 vNormal;
+varying vec3 vFragPos;
 
+void main() {
+    vNormal = gl_NormalMatrix * gl_Normal;
+    vFragPos = vec3(gl_ModelViewMatrix * gl_Vertex);
+    gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+}
