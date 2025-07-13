b0020d313438f893eb8f588a09ab824f3c310025
ShivamKR12 <161696093+ShivamKR12@users.noreply.github.com>
2025-07-13 23:37:49 +0530

    Update shader.py

--- pygame3d/core/shader.py
+++ pygame3d/core/shader.py
@@ -1,1 +1,30 @@
+from OpenGL.GL import *
+
+class Shader:
+    def __init__(self, vertex_path, fragment_path):
+        with open(vertex_path, 'r') as f:
+            vertex_src = f.read()
+        with open(fragment_path, 'r') as f:
+            fragment_src = f.read()
+
+        self.program = glCreateProgram()
+        vertex_shader = self.compile_shader(vertex_src, GL_VERTEX_SHADER)
+        fragment_shader = self.compile_shader(fragment_src, GL_FRAGMENT_SHADER)
+        glAttachShader(self.program, vertex_shader)
+        glAttachShader(self.program, fragment_shader)
+        glLinkProgram(self.program)
+
+        if glGetProgramiv(self.program, GL_LINK_STATUS) != GL_TRUE:
+            raise RuntimeError(glGetProgramInfoLog(self.program).decode())
+
+    def compile_shader(self, source, shader_type):
+        shader = glCreateShader(shader_type)
+        glShaderSource(shader, source)
+        glCompileShader(shader)
+        if glGetShaderiv(shader, GL_COMPILE_STATUS) != GL_TRUE:
+            raise RuntimeError(glGetShaderInfoLog(shader).decode())
+        return shader
+
+    def use(self):
+        glUseProgram(self.program)
 
