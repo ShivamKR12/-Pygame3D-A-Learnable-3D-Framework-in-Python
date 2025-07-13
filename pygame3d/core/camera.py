e0a21efa92b902b50dee693e31317029d8c3c769
ShivamKR12 <161696093+ShivamKR12@users.noreply.github.com>
2025-07-13 23:36:53 +0530

    Update camera.py

--- pygame3d/core/camera.py
+++ pygame3d/core/camera.py
@@ -1,1 +1,18 @@
+from OpenGL.GL import *
+from OpenGL.GLU import *
+import numpy as np
+
+class Camera:
+    def __init__(self):
+        self.position = np.array([0, 0, -5], dtype=np.float32)
+        self.target = np.array([0, 0, 0], dtype=np.float32)
+        self.up = np.array([0, 1, 0], dtype=np.float32)
+
+    def update(self):
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        gluPerspective(45, 800 / 600, 0.1, 100.0)
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+        gluLookAt(*self.position, *self.target, *self.up)
 
